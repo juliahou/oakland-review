7980837ea642028b894c75ef462c35e69a038130
juliahou8 <jjhou@andrew.cmu.edu>
2017-08-24 23:22:17 -0400

    Login for admin page works

--- models.py
+++ models.py
@@ -1,16 +1,14 @@
 #!flask/bin/python
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import flask_admin as admin
+import flask_login as login
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required, login_user, logout_user
 from flask_migrate import Migrate
 import click
-
-class MyView(BaseView):
-    def is_accessible(self):
-        return login.current_user.is_authenticated()
 
 app = Flask(__name__)
 
@@ -20,6 +18,7 @@ login_manager.init_app(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+app.config['SECRET_KEY'] = 'meow'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -65,7 +64,14 @@ class Issue(db.Model):
     def __repr__(self):
         return '<Issue %d>' % self.num
 
-admin = Admin(app, name='Admin', template_mode='bootstrap3')
+class MyAdminIndexView(admin.AdminIndexView):
+    @expose('/')
+    def index(self):
+        if not login.current_user.is_authenticated:
+            return render_template('login.html')
+        return super(MyAdminIndexView, self).index()
+
+admin = Admin(app, name='Admin', index_view=MyAdminIndexView(), base_template="admin/admin.html", template_mode='bootstrap3')
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Issue, db.session))
 
@@ -74,7 +80,7 @@ def init_db():
     db.create_all()
 
     # Create a test user
-    new_user = User('jjhou@andrew.cmu.edu', 'Julia Hou', 'admin')
+    new_user = User('jjhou@andrew.cmu.edu', 'jjhou', 'admin')
     new_user.display_name = 'Julia'
     db.session.add(new_user)
     db.session.commit()
@@ -85,6 +91,11 @@ def init_db():
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+@app.route('/admin')
+@login_required
+def admin():
+    pass
 
 @app.route('/')
 def hello_world():
@@ -107,15 +118,19 @@ def show_issue(issue_id):
     return render_template('issue.html', issue_id=issue_id)
 
 @app.route('/login',methods=['GET','POST'])
-def login():
+def signin():
     if request.method == 'GET':
         return render_template('login.html')
     username = request.form['username']
     password = request.form['password']
     registered_user = User.query.filter_by(username=username,password=password).first()
     if registered_user is None:
-        flash('Username or Password is invalid' , 'error')
-        return redirect(url_for('login'))
+        # flash('Username or Password is invalid' , 'error')
+        return render_template('login.html')
     login_user(registered_user)
-    flash('Logged in successfully')
-    return redirect(url_for('admin'))
+    return redirect(url_for('admin.index'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return render_template('login.html')
